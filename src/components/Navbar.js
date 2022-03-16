26c533ed7bdf2f8091714135d4ea6e6507d37319
wosakrishna <wosa.krishna#gmail.com>
2022-03-16 16:28:37 +0530

    lattt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import { Link, useLocation } from "react-router-dom";
-
+import { FaUserTie} from 'react-icons/fa';
   
 
 const Navbar = () => {
@@ -11,15 +11,16 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">PTE PORTAL</Link>
+                <Link className="navbar-brand" to="/"><h2><b>PTE PORTAL</b></h2></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item ">
-                            <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">DashBoard</Link>
+                            {/* <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">DashBoard</Link> */}
                         </li>
+                        
                         {/* <li className="nav-item">
                             <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} to="/about">Practice </Link>
                         </li> */}
@@ -50,7 +51,12 @@ const Navbar = () => {
            
 
                     </ul>
-                  
+                    <span class="navbar-text text-white  ">
+                  <u> Welcome, Krishna  </u>
+      </span>
+                    <span class="navbar-text mx-3 ">
+                    <Link className={`text-info nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">DashBoard</Link>
+      </span><span className="navbar-text mx-3 boader boader-info"><button className='rounded-circle'><FaUserTie/></button></span>
                 </div>
             </div>
         </nav>
